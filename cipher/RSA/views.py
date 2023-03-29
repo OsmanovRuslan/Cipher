e853df6dc20d35a584d51ba39bd3c05cb470880d
Ruslan <114252945+OsmanovRuslan@users.noreply.github.com>
2023-03-29 12:53:26 +0300

    Initial commit

---
+++ cipher/RSA/views.py
@@ -0,0 +1,41 @@
+import binascii
+from django.shortcuts import render
+import rsa
+
+
+# Create your views here.
+user_message = ""
+def second(request):
+    global user_message
+    if request.method == "POST":
+        key_pub, key_priv = keys()
+        if "crypto" in request.POST:
+            user_message = request.POST["crypto_text"]
+            answer = {
+                "ans": crypto(user_message, key_pub),
+                "start": key_pub
+            }
+        elif "decrypto" in request.POST:
+            answer = {
+                "ans": decrypto(user_message, key_priv),
+                "start": key_priv
+            }
+    else:
+        answer = {}
+    return render(request, "second.html", answer)
+
+
+def keys():
+    (_keyPublic, _keyPrivate) = rsa.newkeys(512)
+    return _keyPublic, _keyPrivate
+
+
+def crypto(message, key_public):
+    msg = message.encode("utf-8")
+    crpt = rsa.encrypt(msg, key_public)
+    return crpt
+
+
+def decrypto(message, key_private):
+    dcrpt = rsa.decrypt(message, key_private)
+    return dcrpt
---
+++ cipher/doing/views.py
@@ -0,0 +1,91 @@
+from django.shortcuts import render
+import numpy as np
+# Create your views here.
+
+
+def doing(request):
+    if request.method == 'POST':
+
+        message = request.POST.get("message", "")
+        length = request.POST.get("lenght", "")
+        key = generate_key_matrix(int(length))
+        encoded_message = encode(message, key)
+        decoded_message = "Ошибка. Повторите запрос"
+        try:
+            decoded_message = decode(encoded_message, key)
+        except ValueError:
+            doing(request)
+
+        data = {
+            "message": message,
+            "encoded_message": encoded_message,
+            "decoded_message": decoded_message
+        }
+    else:
+        data = {}
+    return render(request, 'cipher.html', data)
+
+
+def generate_key_matrix(size):
+    while True:
+        # генерируем случайную квадратную матрицу заданного размера
+        key_matrix = np.random.randint(0, 256, size=(size, size), dtype=np.uint8)
+
+        # проверяем, что определитель матрицы не равен нулю и не имеет общих делителей с размером алфавита (256)
+        if np.linalg.det(key_matrix) != 0 and np.gcd(int(np.linalg.det(key_matrix)), 256) == 1:
+            break
+
+    # вычисляем обратную матрицу с помощью LU-разложения
+    inv_key_matrix = np.linalg.inv(key_matrix)
+
+    # приводим элементы матрицы к типу uint8 и по модулю 256
+    key_matrix = np.mod(key_matrix.astype(np.uint8), 256)
+    inv_key_matrix = np.mod(inv_key_matrix.astype(np.uint8), 256)
+
+    return key_matrix
+
+
+def determinant(matrix):
+    return int(round(np.linalg.det(matrix)))
+
+
+def inverse_matrix_mod_256(matrix):
+    det = determinant(matrix)
+    if det == 0:
+        raise ValueError("Matrix is not invertible")
+    det_inv = pow(det, -1, 256)
+    adj = np.round(det_inv * np.linalg.inv(matrix) * det) % 256
+    return adj.astype(int)
+
+
+def encode(message, key):
+    message_size = len(message)
+    block_size = len(key)
+    key_matrix = np.array(key)
+    blocks = [message[i:i + block_size] for i in range(0, message_size, block_size)]
+    encoded_blocks = []
+    for block in blocks:
+        if len(block) < block_size:
+            block += ' ' * (block_size - len(block))  # добавление пробелов
+        block_matrix = np.array([ord(char) for char in block])
+        block_matrix = block_matrix.reshape(-1, block_size)
+        encoded_block_matrix = np.matmul(block_matrix, key_matrix) % 256
+        encoded_block = "".join([chr(char) for char in encoded_block_matrix.reshape(-1)])
+        encoded_blocks.append(encoded_block)
+    encoded_message = "".join(encoded_blocks)
+    return encoded_message
+
+
+def decode(message, key):
+    message_size = len(message)
+    block_size = len(key)
+    key_matrix = np.array(key)
+    blocks = [message[i:i + block_size] for i in range(0, message_size, block_size)]
+    decoded_blocks = []
+    for block in blocks:
+        block_matrix = np.array([ord(char) for char in block])
+        decoded_block_matrix = np.matmul(block_matrix, inverse_matrix_mod_256(key_matrix)) % 256
+        decoded_block = "".join([chr(char) for char in decoded_block_matrix.reshape(-1)])
+        decoded_blocks.append(decoded_block)
+    decoded_message = "".join(decoded_blocks).rstrip()  # убираем лишние пробелы в конце
+    return decoded_message
